a4faf9bd53322be5ac7ad6cf33e9bfe3c26f69ca
Mike Gregory <mgregory8219@gmail.com>
2016-05-11 22:50:41 -0400

    created destroy method for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   	@user = User.find_by(username: params[:username]).try(:authenticate, params[:password])
 
   	return render action: 'new' unless @user
-
+    session[:comedian_id] = nil
   	session[:user_id] = @user.id
   	p session
   	redirect_to root_path
@@ -17,8 +17,14 @@ class SessionsController < ApplicationController
   	@comedian = Comedian.find_by(username: params[:username]).try(:authenticate, params[:password])
 
   	return render action: 'new' unless @comedian
-
+    session[:user_id] = nil
   	session[:comedian_id] = @comedian.id
   	redirect_to root_path
+  end
+
+  def destroy
+    session[:user_id] = nil
+    session[:comedian_id] = nil
+    redirect_to root_url, :notice => "Logged out!"
   end
 end
