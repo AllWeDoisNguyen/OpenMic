b6049979858fc71d4861179f1aa9cbdf702c8575
Mike Gregory <mgregory8219@gmail.com>
2016-05-14 15:51:15 -0400

    worked on paperclip / image won't show issue

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -3,6 +3,6 @@ class Comedian < ActiveRecord::Base
 	validates :name, :username, :password_digest, presence: true
 	has_many :bookings
 	has_many :shows, :through => :bookings
-	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/OpenMiic/humor-public-speaking.jpg"
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "OpenMiic/humor-public-speaking.jpg", :default_style => :thumb
 	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
