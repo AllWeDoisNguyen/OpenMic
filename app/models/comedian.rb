02abe8fff3aabb5154be0f40faac5812d2bcbe76
Mike Gregory <mgregory8219@gmail.com>
2016-05-16 00:00:03 -0400

    homepage styling and new comedian form improvements

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,9 +1,10 @@
 class Comedian < ActiveRecord::Base
 	has_secure_password
-	validates :name, :username, :password_digest, presence: true
 	has_many :bookings
 	has_many :shows, :through => :bookings
 	has_attached_file :avatar, styles: { medium: "300x300#", thumb: "100x100#" }, default_url: "OpenMiic/humor-public-speaking-300px.jpg", :default_style => :thumb
+	validates :name, :username, :password_digest, presence: true
+	validates_length_of :bio, maximum: 160
 	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
 	def next_show
