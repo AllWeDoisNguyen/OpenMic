20a3fbc362069a7ce63248793c09c2656a0c145f
Mike Gregory <mgregory8219@gmail.com>
2016-05-18 00:08:13 -0400

    added javascript comedian panels to homepage

--- app/assets/javascripts/panels.js
+++ app/assets/javascripts/panels.js
@@ -1,63 +1,49 @@
 $(document).ready(function() {
-    var panels = $('.user-infos');
-    var panelsButton = $('.dropdown-user');
-    panels.hide();
-
-    //Click dropdown
-    panelsButton.click(function() {
-        //get data-for attribute
-        var dataFor = $(this).attr('data-for');
-        var idFor = $(dataFor);
-
-        //current button
-        var currentButton = $(this);
-        idFor.slideToggle(400, function() {
-            //Completed slidetoggle
-            if(idFor.is(':visible'))
-            {
-                currentButton.html('<i class="glyphicon glyphicon-chevron-up text-muted"></i>');
-            }
-            else
-            {
-                currentButton.html('<i class="glyphicon glyphicon-chevron-down text-muted"></i>');
-            }
-        })
-    });
-
-
-    $('[data-toggle="tooltip"]').tooltip();
-
-
-// Demo code beg
-    $('button').click(function(e) {
-        e.preventDefault();
-        alert("This is a demo.\n :-)");
-    });
-// Demo code end
-
-});
-
-$(document).ready(function() {
+  var timed_actions
   $('.comedian-hover-box').hover(function() {
-      // On MouseOver Actions for Comedian Panels
-      $(this).addClass('col-lg-5 comedian-panel-container-outer-hover')
+    var comedianHoverBox = $(this)
+    // On MouseOver Actions for Comedian Panels
+    comedianHoverBox.addClass('col-lg-5 comedian-panel-container-outer-hover')
       .removeClass('col-lg-2 comedian-panel-container-outer');
-      $(this).find('.comedian-panel-container-body')
-        .addClass('comedian-panel-container-body-hover')
-        .removeClass('comedian-panel-container-body');
-      $(this).find('.comedian-panel-container-inner')
-        .addClass('comedian-panel-container-inner-hover')
-        .removeClass('comedian-panel-container-inner');
+    comedianHoverBox.find('.comedian-panel-container-body')
+      .addClass('comedian-panel-container-body-hover')
+      .removeClass('comedian-panel-container-body');
+    comedianHoverBox.find('.comedian-panel-container-inner')
+      .addClass('comedian-panel-container-inner-hover')
+      .removeClass('comedian-panel-container-inner');
+    comedianHoverBox.find('.comedian-panel-body-line')
+      .addClass('comedian-panel-body-line-hover')
+      .removeClass('comedian-panel-body-line')
+    comedianHoverBox.find('vert-flex-box')
+      .addClass('col-lg-6')
+      .removeClass('col-lg-12');
+    timed_actions = window.setTimeout(function() {
+      comedianHoverBox.find('.hidden')
+        .removeClass('hidden')
+        .addClass('was-hidden');
+    }, 2000);
   }, function() {
-      // On MouseLeave Actions for Comedian Panels
-      $(this).removeClass('col-lg-5 comedian-panel-container-outer-hover')
+    var comedianHoverBox = $(this)
+
+    // On MouseLeave Actions for Comedian Panels
+    comedianHoverBox.find('.was-hidden')
+      .removeClass('was-hidden')
+      .addClass('hidden');
+    comedianHoverBox.removeClass('col-lg-5 comedian-panel-container-outer-hover')
       .addClass('col-lg-2 comedian-panel-container-outer');
-      $(this).find('.comedian-panel-container-body-hover')
-        .removeClass('comedian-panel-container-body-hover')
-        .addClass('comedian-panel-container-body');
-      $(this).find('.comedian-panel-container-inner-hover')
-        .removeClass('comedian-panel-container-inner-hover')
-        .addClass('comedian-panel-container-inner');
-    });
+    comedianHoverBox.find('.comedian-panel-container-body-hover')
+      .removeClass('comedian-panel-container-body-hover')
+      .addClass('comedian-panel-container-body');
+    comedianHoverBox.find('.comedian-panel-container-inner-hover')
+      .removeClass('comedian-panel-container-inner-hover')
+      .addClass('comedian-panel-container-inner');
+    comedianHoverBox.find('.comedian-panel-body-line-hover')
+      .addClass('comedian-panel-body-line')
+      .removeClass('comedian-panel-body-line-hover')
+    comedianHoverBox.find('vert-flex-box')
+      .addClass('col-lg-12')
+      .removeClass('col-lg-6');
+    window.clearTimeout(timed_actions);
+  });
 
 });
