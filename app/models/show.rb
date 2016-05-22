e56022e0bab1f06eca5226f275b159ef2e2cb9d5
Mike Gregory <mgregory8219@gmail.com>
2016-05-22 02:28:29 -0400

    basic outline for show show page

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,6 @@
 class Show < ActiveRecord::Base
 	has_many :bookings
+	has_many :reviews
 	has_many :comedians, :through => :bookings
 	has_many :users, :through => :bookings
 	belongs_to :owner, class_name: "User", foreign_key: "user_id"
